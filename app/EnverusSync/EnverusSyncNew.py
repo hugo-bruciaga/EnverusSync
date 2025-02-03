0c7e15d88071a92509fac5cf586e373ca7bbad17
Hugo Bruciaga <hbruciaga@anteroresources.com>
2025-02-03 10:53:02 -0700

    EnverusSyncNew: added connection function

--- app/EnverusSync/EnverusSyncNew.py
+++ app/EnverusSync/EnverusSyncNew.py
@@ -1,6 +1,25 @@
+import datetime, pyodbc
 
-from Enverus.enverus_sync import db_connect
-import datetime
+def db_connect(
+        server: str,
+        database: str,
+        login_name: str = None,
+        password: str = None,
+        context: str = 'js.utils',
+        driver_ver: int = 17
+):
+    database = 'master' if database is None or database == '' else database
+    connection_string = f'DRIVER={{ODBC Driver {driver_ver} for SQL Server}};' \
+                        f'SERVER={server};' \
+                        f'DATABASE={database};' \
+                        f'APP={context};' \
+                        f'TrustServerCertificate=yes;'
+
+    connection_string += 'Trusted_Connection=yes;' \
+        if login_name is None or password is None or login_name == '' or password == '' \
+        else f'UID={login_name};PWD={password}'
+
+    return pyodbc.connect(connection_string)
 
 cn = db_connect('biconnect.enverus.com', 'DrillingInfo_DIBI', 'env_2866', 'YRwmC1u5XSDa')
 cr = cn.cursor()
